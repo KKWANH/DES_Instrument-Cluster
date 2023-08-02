b3556e7d5ad7f8525b4090832dd4f94150760be3
KKWANH <kwanho0096@gmail.com>
2023-08-02 15:02:33 +0200

    :spakles: #10 [speedometer] dbus receiver

--- dashboard/main.cpp
+++ dashboard/main.cpp
@@ -2,6 +2,7 @@
 # include   <QtQml/QQmlApplicationEngine>
 # include   <QtGui/QFont>
 # include   <QtGui/QFontDatabase>
+# include   <QtDBus/QtDBus>
 
 # define    FAILURE -1
 # define    SUCCESS 0
@@ -10,13 +11,41 @@ int         main(
     int     _arc,
     char*   _arv[]
 ) {
-    QGuiApplication app(_arc, _arv);
+    // application define
+    QGuiApplication         _app(_arc, _arv);
 
+    // font setting
     QFontDatabase::addApplicationFont(":/asset/fonts/DejaVuSans.ttf");
-    app.setFont(QFont("DejaVu Sans"));
+    _app.setFont(QFont("DejaVu Sans"));
 
-    QQmlApplicationEngine engine(QUrl("qrc:/asset/qml/dashboard.qml"));
-    if (engine.rootObjects().isEmpty())
+    // dbus connection test
+    if (!QDBusConnection::sessionBus().isConnected())
+    {
+        fprintf(
+            stderr,
+            "[Error] [Dashboard] [DBus Connection]  Cannot connect to the D-Bus session bus.\n",
+            "                                       To start, run : [eval `dbus-launch --auto-syntax]"
+        );
+        return ;
+    }
+
+    // dbus signal receiver
+    DBusSignalHandler       _signal_handler;
+
+    // connect dbus signal to slot
+    QDBusConnection::sessionBus().connect(
+        QString(),              // Service
+        "/org/example/Test",    // Path
+        "org.example.Test"      // Interface
+        "TestSignal",           // Signal Name
+        &_signal_handler,
+        SLOT(handleSignal(QDBusMessage))
+    );
+
+    // qml on
+    QQmlApplicationEngine   _engine(QUrl("qrc:/asset/qml/dashboard.qml"));
+    if (_engine.rootObjects().isEmpty())
         return (FAILURE);
-    return (app.exec());
+
+    return (_app.exec());
 }
